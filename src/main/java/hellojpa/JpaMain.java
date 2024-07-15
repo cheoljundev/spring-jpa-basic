3b7bd146d0eea0f7f0528225f77f34d3c86f1534
cheoljundev <cheoljundev@gmail.com>
2024-07-15 20:46:04 +0900

    영속성 컨텍스트 2 : 영속성 엔티티의 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,20 +15,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJpa");
 
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
+            // 영속 엔티티의 동일성 보장
+            System.out.println("result = " + (findMember1 ==  findMember2));
 
             tx.commit();
         } catch (Exception e) {
