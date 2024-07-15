2775ee6e502a6b3b9ef1831e6dd0f6eeb9ef8d0e
cheoljundev <cheoljundev@gmail.com>
2024-07-15 22:31:55 +0900

    기본 키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,27 +2,13 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
-
     @Entity
     public class Member {
-        @Id
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
-        @Column(name = "name", insertable = true,
-                updatable = true, nullable = true, unique = false, length = 10)
-        private String username;
-        private Integer age;
-        @Enumerated(EnumType.STRING) // 필수로 STRING으로 써야 한다.
-        private RoleType roleType;
-        @Temporal(TemporalType.TIMESTAMP) // Temporal은 자바8 이상부터는 LocalDate, LocalDateTime등 등장으로 필요없어짐.
-        private Date createdDate;
-        @Temporal(TemporalType.TIMESTAMP)
-        private Date lastModifiedDate;
-        @Lob // clob
-        private String description;
 
-        @Transient // 매핑하지 않음
-        private int temp;
+        @Column(name = "name")
+        private String username;
 
         public Member() {
         }
@@ -35,27 +21,4 @@ import java.util.Date;
             this.username = username;
         }
 
-        public void setAge(Integer age) {
-            this.age = age;
-        }
-
-        public void setRoleType(RoleType roleType) {
-            this.roleType = roleType;
-        }
-
-        public void setCreatedDate(Date createdDate) {
-            this.createdDate = createdDate;
-        }
-
-        public void setLastModifiedDate(Date lastModifiedDate) {
-            this.lastModifiedDate = lastModifiedDate;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public void setTemp(int temp) {
-            this.temp = temp;
-        }
     }
