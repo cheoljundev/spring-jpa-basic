cdd9df314e0b221e1f3a06ca10ca5861ecc6f652
cheoljundev <cheoljundev@gmail.com>
2024-07-15 23:27:42 +0900

    객체를 테이블에 맞추어 모델링시 문제점

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,8 +2,6 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -16,10 +14,18 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("B");
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId());
             em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,20 +5,36 @@ import jakarta.persistence.*;
     @Entity
     public class Member {
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "member_id")
         private Long id;
 
         @Column(name = "name")
         private String username;
 
-        public Member() {
+        @Column(name = "team_id")
+        private Long teamId;
+
+        public Long getId() {
+            return id;
         }
 
         public void setId(Long id) {
             this.id = id;
         }
 
+        public String getUsername() {
+            return username;
+        }
+
         public void setUsername(String username) {
             this.username = username;
         }
 
+        public Long getTeamId() {
+            return teamId;
+        }
+
+        public void setTeamId(Long teamId) {
+            this.teamId = teamId;
+        }
     }
