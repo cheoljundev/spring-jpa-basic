39899783d7b0f8f7a9a2fa0188e1e16fb7fc1be1
cheoljundev <cheoljundev@gmail.com>
2024-07-16 04:58:15 +0900

    영속성 전이(cascade)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,53 +6,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-    public class Member extends BaseEntity{
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "member_id")
-        private Long id;
+public class Member extends BaseEntity{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
 
-        @Column(name = "name")
-        private String username;
+    @Column(name = "name")
+    private String username;
 
-        @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
 //        @ManyToOne(fetch = FetchType.EAGER)
-        @JoinColumn(name = "TEAM_ID") // N쪽이 주인
-        private Team team;
+    @JoinColumn(name = "TEAM_ID") // N쪽이 주인
+    private Team team;
 
-        @OneToOne
-        @JoinColumn(name = "LOCKER_ID")
-        private Locker locker;
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
-        @OneToMany(mappedBy = "member")
-        private List<MemberProduct> memberProducts = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public Team getTeam() {
-            return team;
-        }
-
-        public void setTeam(Team team) {
-            this.team = team;
-        }
-
-        // == 연관관계 편의 메서드 ==//
-        public void changeTeam(Team team) {
-            this.team = team;
-            team.getMembers().add(this);
-        }
+    public Long getId() {
+        return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    // == 연관관계 편의 메서드 ==//
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
